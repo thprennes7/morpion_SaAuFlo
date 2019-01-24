faf383995f32f599ba24c317268f68bd9ec356ad
Salome Marzin <salome.mrzn@gmail.com>
2019-01-24 17:27:18 +0100

    add emojis and instructions

--- lib/app/rooter.rb
+++ lib/app/rooter.rb
@@ -8,10 +8,14 @@ class Rooter
   def perform
     #Début du jeu, les joueurs rentrent leurs noms
     system('clear')
-    puts 'Bonjour ! Bienvenue dans le jeu du Morpion, veuillez renseigner le nom des joueurs !'
-    print 'Joueur 1 x : '
+    puts 'Bonjour ! Bienvenue dans le jeu du Morpion,
+    Voici comment y jouer : Les joueurs inscrivent tour à tour leur symbole sur une grille.
+    Le premier qui parvient à aligner trois de ses symboles horizontalement, verticalement ou en diagonale remporte la partie.
+    
+    Commençons à jouer ! Veuillez renseigner le nom des joueurs !'
+    print "Joueur 1 #{Emoji.find_by_alias("x").raw} : "
     @player_1 = gets.chomp
-    print 'Joueur 2 o : '
+    print "Joueur 2 #{Emoji.find_by_alias("o").raw} : "
     @player_2 = gets.chomp
     puts "Bienvenue #{@player_1} et #{@player_2}, tenez vous prêt, c'est parti !"
 
@@ -27,7 +31,7 @@ class Rooter
         perform
         return true
       }
-      menu.choice 'Recommencer une partie avec les même joueur', -> { 
+      menu.choice 'Recommencer une partie avec les mêmes joueur', -> { 
         @controller.new_game(@player_1, @player_2) 
         return true
       }
